d63a43a1057f5ae93783b7be014ade6992e65f85
Andrey <avdanilin95@gmail.com>
2021-07-18 19:51:05 +0300

    scripts for modal and some change for view

--- src/js/common.js
+++ src/js/common.js
@@ -1,32 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+    const closedModal = document.querySelectorAll('.go-back')
+    const backdropModal = document.querySelector('.modal-backdrop')
     const modals = document.querySelectorAll('[data-modal]')
 
     modals.forEach(modal => {
         modal.addEventListener('click', toggleModal)
     })
 
+
     function toggleModal() {
         const idModal = this.getAttribute('data-modal')
-        const backdropModal = document.createElement('div')
 
-        backdropModal.classList.add('modal-backdrop')
         backdropModal.style.opacity = 1
-        document.body.style.overflow = 'hidden'
+        backdropModal.style.zIndex = 99
+        document.body.style.overflowY = 'auto'
         document.body.appendChild(backdropModal)
 
         openModal(idModal)
 
-        // backdropModal.addEventListener('click', function () {
-        //     backdropModal.style.opacity = 0
-        //     document.body.style.overflow = ''
-        //
-        //     setTimeout(() => {
-        //         backdropModal.remove()
-        //     }, 400)
-        // })
-        //
-        // closeModal(idModal);
+        backdropModal.addEventListener('click', hideModal)
+
+        closedModal.forEach(closeItem => {
+            closeItem.addEventListener('click', hideModal)
+        })
+
+        function hideModal() {
+            setTimeout(() => {
+                backdropModal.style.opacity = 0
+                backdropModal.style.zIndex = '-99'
+                document.body.style.overflow = ''
+            }, 400)
+
+            closeModal(idModal);
+        }
+
     }
 
     function openModal(id) {
@@ -38,7 +45,37 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.style.position = 'absolute'
         modal.style.top = '50%'
         modal.style.left = '50%'
+        modal.style.opacity = 1
         modal.style.transform = 'translate(-50%, -50%)'
+
+        modal.addEventListener('submit', (e) => {
+            e.preventDefault()
+
+            modal.querySelector('.modal-body').innerHTML = ''
+
+            let successText = ''
+
+            if (id === 'to-the-ass') {
+                modal.querySelector('.modal-header').remove()
+                modal.querySelector('form').style.justifyContent = 'center'
+                successText = 'Спасибо, что поделились!'
+
+            } else {
+                modal.querySelector('.modal-header').style.marginBottom = 50 + 'px'
+                successText = 'Спасибо, что подписались на нашу рассылку!'
+            }
+
+            const success = `<p class="success">${successText}</p>`
+
+            modal.querySelector('.modal-body').innerHTML = ''
+            modal.querySelector('.modal-body').innerHTML = success
+
+            setTimeout(() => {
+                backdropModal.style.opacity = 0
+                modal.style.display = 'none'
+            }, 3000)
+        })
+
     }
 
     function closeModal(id) {
@@ -49,7 +86,6 @@ document.addEventListener('DOMContentLoaded', function () {
         setTimeout(() => {
             modal.style.display = 'none'
         }, 400)
-
     }
 
 })
